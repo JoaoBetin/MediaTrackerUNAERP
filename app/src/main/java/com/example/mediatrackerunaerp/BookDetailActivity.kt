0bd908d3f5015194bbefa21d132cb67a2fcb03b9
JoaoBetin <betinjoao2@gmail.com>
2026-08-19 21:05:15 -0300

    first commit

---
+++ app/src/main/java/com/example/mediatrackerunaerp/BookDetailActivity.kt
@@ -0,0 +1,29 @@
+package com.example.mediatrackerunaerp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.mediatrackerunaerp.ui.theme.MediaTrackerUNAERPTheme
+
+class BookDetailActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.book_detail_layout)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)){ v, insets ->
+            val systemBar = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBar.left, systemBar.top,systemBar.right, systemBar.bottom )
+            insets
+        }
+    }
+}
